657a55c5fc2162464f30b004651d59a7e7361754
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 09:34:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "build-${var.build}" 
+  name = Jenkins
 
   instance_type          = "t2.micro"
   key_name               = "ansible"
