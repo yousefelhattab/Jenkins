f792f0dfe19e218535553b88814c403d3785c622
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 10:44:06 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ module "ec2_instance" {
   vpc_security_group_ids = ["sg-0eb11d9a0361848f3"]
   subnet_id              = "subnet-02e7e46a9acf6627b"
   
-
+  associate_public_ip_address = true  # Automatically associate a public IP
 
   tags = {
     Terraform   = "true"
@@ -37,4 +37,6 @@ module "ec2_instance" {
   }
 }
 
-
+output "ec2_public_ip" {
+  value = module.ec2_instance.public_ip
+}
