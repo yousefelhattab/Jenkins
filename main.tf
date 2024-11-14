9eed46ab3eb24e74ec2f1eb0eda4b6c2e02c55f5
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 10:50:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,29 +10,19 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-# Define the variable for the instance name
-variable "instance_name" {
-  description = "The name of the EC2 instance"
-  type        = string
-}
-
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  # Use the instance_name variable passed from the Jenkins pipeline
-  name = var.instance_name
+  name = "Jenkins"
 
   instance_type          = "t2.micro"
   key_name               = "ansible"
   
   vpc_security_group_ids = ["sg-0eb11d9a0361848f3"]
   subnet_id              = "subnet-02e7e46a9acf6627b"
-  
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
 }
-
