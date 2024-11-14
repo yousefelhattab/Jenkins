d8863759532d14e01df16a544d226ec0f5585d9e
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 09:31:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "t1"
+  name = "build-${var.build}" 
 
   instance_type          = "t2.micro"
   key_name               = "ansible"
