f518e730356e9cde3dd26d64f10e2629e47efb6c
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 10:41:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Define the variable for the instance name
+variable "instance_name" {
+  description = "The name of the EC2 instance"
+  type        = string
+}
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "Jenkins"
+  # Use the instance_name variable passed from the Jenkins pipeline
+  name = var.instance_name
 
   instance_type          = "t2.micro"
   key_name               = "ansible"
@@ -22,7 +29,7 @@ module "ec2_instance" {
   vpc_security_group_ids = ["sg-0eb11d9a0361848f3"]
   subnet_id              = "subnet-02e7e46a9acf6627b"
   
-  associate_public_ip_address = true  # Automatically associate a public IP
+
 
   tags = {
     Terraform   = "true"
@@ -30,6 +37,4 @@ module "ec2_instance" {
   }
 }
 
-output "ec2_public_ip" {
-  value = module.ec2_instance.public_ip
-}
+
