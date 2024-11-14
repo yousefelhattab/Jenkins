0666a821a164436241db523d6be51e5177867a2f
Yousef Waleed Elhattab <137096519+yousefelhattab@users.noreply.github.com>
2024-11-14 09:54:25 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 terraform {
   required_providers {
     aws = {
@@ -11,6 +10,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
@@ -21,10 +21,15 @@ module "ec2_instance" {
   
   vpc_security_group_ids = ["sg-0eb11d9a0361848f3"]
   subnet_id              = "subnet-02e7e46a9acf6627b"
-  associate_public_ip_address = true
+  
+  associate_public_ip_address = true  # Automatically associate a public IP
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+}
+
+output "ec2_public_ip" {
+  value = module.ec2_instance.public_ip
 }
